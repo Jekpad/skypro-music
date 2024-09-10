4b626789337143a5e606299b7c1b5537468550f2
J[A]shy <jekpadt@gmail.com>
2024-09-10 22:04:18 +0300

    Add tests

--- jest.config.ts
+++ jest.config.ts
@@ -97,7 +97,9 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
   moduleNameMapper: {
-    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@/store/(.*)$": "<rootDir>/src/store/$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
